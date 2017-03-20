179484c57c4a13c948d352bb6445c656b5258b97
Nathan Cahill <nathan@nathancahill.com>
2017-03-20 17:43:16 -0500

    add deploy to frontend

--- rollup.config.js
+++ rollup.config.js
@@ -1,42 +1,55 @@
 
+import process from 'process'
 import buble from 'rollup-plugin-buble'
 import commonjs from 'rollup-plugin-commonjs'
 import nodeResolve from 'rollup-plugin-node-resolve'
 import sass from 'rollup-plugin-sass'
 import replace from 'rollup-plugin-replace'
-import uglify from 'rollup-plugin-uglify'
 import autoprefixer from 'autoprefixer'
 import postcss from 'postcss'
+import hash from 'rollup-plugin-hash'
+
+const plugins = [
+    buble({
+        exclude: '**/*.scss',
+        objectAssign: 'Object.assign',
+    }),
+    commonjs({
+        include: 'node_modules/**',
+        namedExports: {
+            'node_modules/react/react.js': ['Component', 'PropTypes', 'Children', 'createElement'],
+        },
+    }),
+    nodeResolve({
+        browser: true,
+    }),
+    replace({
+        'process.env.NODE_ENV': JSON.stringify('dev'),
+    }),
+    sass({
+        output: true,
+        options: {
+            includePaths: ['node_modules'],
+        },
+        processor: css => postcss([autoprefixer])
+            .process(css)
+            .then(result => result.css),
+    }),
+]
+
+if (process.env.NODE_ENV === 'production') {
+    plugins.push(
+    hash({
+        replace: true,
+        dest: 'dist/bundle.[hash].js',
+        manifest: 'dist/manifest.json',
+        manifestKey: 'bundle',
+    }))
+}
 
 export default {
     entry: 'index.js',
     format: 'iife',
-    plugins: [
-        buble({
-            exclude: '**/*.scss',
-            objectAssign: 'Object.assign',
-        }),
-        commonjs({
-            include: 'node_modules/**',
-            namedExports: {
-                'node_modules/react/react.js': ['Component', 'PropTypes', 'Children', 'createElement'],
-            },
-        }),
-        nodeResolve({
-            browser: true,
-        }),
-        replace({
-            'process.env.NODE_ENV': JSON.stringify('dev'),
-        }),
-        sass({
-            output: true,
-            options: {
-                includePaths: ['node_modules'],
-            },
-            processor: css => postcss([autoprefixer])
-                .process(css)
-                .then(result => result.css),
-        }),
-    ],
     dest: 'dist/bundle.js',
+    plugins,
 }
