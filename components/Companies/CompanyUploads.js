5267afa6e0b01bd9d104528b05be627ca1d54089
Nathan Cahill <nathan@nathancahill.com>
2017-03-22 09:27:27 -0600

    add correct relationships

--- components/Companies/CompanyUploads.js
+++ components/Companies/CompanyUploads.js
@@ -19,17 +19,22 @@ class CompanyUploads extends React.Component {
     }
 
     onUpload (obj) {
-        obj.filesUploaded.map(file => {
-            console.log(file)
+        const lab = this.props.labs
+            .filter(fLab => fLab.get('url') === this.props.currentLabUrl)
+            .first()
+
+        obj.filesUploaded.map(file =>
             this.props.createUpload({
                 filename: file.name,
                 url: file.url,
-            })
-        })
+                lab_id: lab.get('id'),
+                company_id: this.props.company.get('id'),
+            }))
     }
 
     render () {
         const uploads = this.props.uploads
+            .filter(upload => upload.get('company_id') === this.props.company.get('id'))
             .sort((a, b) => a.get('id') - b.get('id'))
             .entrySeq()
 
@@ -58,7 +63,7 @@ class CompanyUploads extends React.Component {
                             <tr key={id}>
                                 <td>{upload.get('filename')}</td>
                                 <td>{new timeago().format(new Date(upload.get('created_at')))}</td>
-                                <td>New</td>
+                                <td>{upload.get('sent') ? 'Sent' : 'New'}</td>
                             </tr>
                         ))}
                     </tbody>
@@ -70,6 +75,8 @@ class CompanyUploads extends React.Component {
 
 const mapStateToProps = store => ({
     uploads: store.get('uploads'),
+    labs: store.get('labs'),
+    currentLabUrl: store.get('currentLabUrl'),
 })
 
 const mapDispatchToProps = dispatch => ({
