0d708d59c2173e950f84f4146880cf6aa1f5a9c6
Nathan Cahill <nathan@nathancahill.com>
2017-04-03 12:16:12 -0700

    add reporting menu

--- components/SiteApp.js
+++ components/SiteApp.js
@@ -48,10 +48,119 @@ class SiteApp extends React.Component {
                             activeClassName="active"
                         >Downloads</NavLink>
                         <NavLink
+                            to={`/app/sites/${site.get('id')}/pre-sampling`}
+                            className="nav-link"
+                            activeClassName="active"
+                        >Pre-Sampling Reports</NavLink>
+                        <PrivateRoute
+                            path={`/app/sites/${site.get('id')}/pre-sampling`}
+                            component={() => (
+                                <nav className="nav nav-pills flex-column" style={{ marginLeft: 20 }}>
+                                    <NavLink
+                                        to={`/app/sites/${site.get('id')}/pre-sampling/scope`}
+                                        className="nav-link"
+                                        activeClassName="active"
+                                    >Work Scope</NavLink>
+                                    <NavLink
+                                        to={`/app/sites/${site.get('id')}/pre-sampling/sample-bottle-request`}
+                                        className="nav-link"
+                                        activeClassName="active"
+                                    >Sample Bottle Request Report</NavLink>
+                                    <NavLink
+                                        to={`/app/sites/${site.get('id')}/pre-sampling/site-map`}
+                                        className="nav-link"
+                                        activeClassName="active"
+                                    >Site Map</NavLink>
+                                    <NavLink
+                                        to={`/app/sites/${site.get('id')}/pre-sampling/on-site-activity`}
+                                        className="nav-link"
+                                        activeClassName="active"
+                                    >Notice of On-site Work Activity</NavLink>
+                                    <NavLink
+                                        to={`/app/sites/${site.get('id')}/pre-sampling/chain-custody`}
+                                        className="nav-link"
+                                        activeClassName="active"
+                                    >Chain of Custody</NavLink>
+                                    <NavLink
+                                        to={`/app/sites/${site.get('id')}/pre-sampling/sample-bottle-labels`}
+                                        className="nav-link"
+                                        activeClassName="active"
+                                    >Sample Bottle Labels</NavLink>
+                                    <NavLink
+                                        to={`/app/sites/${site.get('id')}/pre-sampling/field-data-form`}
+                                        className="nav-link"
+                                        activeClassName="active"
+                                    >Field Data Form</NavLink>
+                                </nav>
+                            )}
+                        />
+                        <NavLink
                             to={`/app/sites/${site.get('id')}/reports`}
                             className="nav-link"
                             activeClassName="active"
-                        >Reports</NavLink>
+                        >Analytical Reports</NavLink>
+                        <PrivateRoute
+                            path={`/app/sites/${site.get('id')}/reports`}
+                            component={() => (
+                                <nav className="nav nav-pills flex-column" style={{ marginLeft: 20 }}>
+                                    <NavLink
+                                        to={`/app/sites/${site.get('id')}/reports/history`}
+                                        className="nav-link"
+                                        activeClassName="active"
+                                    >Site History</NavLink>
+                                    <NavLink
+                                        to={`/app/sites/${site.get('id')}/reports/field-data-table`}
+                                        className="nav-link"
+                                        activeClassName="active"
+                                    >Field Data Results Table</NavLink>
+                                    <NavLink
+                                        to={`/app/sites/${site.get('id')}/reports/field-data-graph`}
+                                        className="nav-link"
+                                        activeClassName="active"
+                                    >Field Data Results Graph</NavLink>
+                                    <NavLink
+                                        to={`/app/sites/${site.get('id')}/reports/groundwater-elevation`}
+                                        className="nav-link"
+                                        activeClassName="active"
+                                    >Groundwater Elevation</NavLink>
+                                    <NavLink
+                                        to={`/app/sites/${site.get('id')}/reports/analytical-results-map`}
+                                        className="nav-link"
+                                        activeClassName="active"
+                                    >Analytical Results Map</NavLink>
+                                    <NavLink
+                                        to={`/app/sites/${site.get('id')}/reports/analytical-results-table`}
+                                        className="nav-link"
+                                        activeClassName="active"
+                                    >Analytical Results Table</NavLink>
+                                    <NavLink
+                                        to={`/app/sites/${site.get('id')}/reports/analytical-results-graph`}
+                                        className="nav-link"
+                                        activeClassName="active"
+                                    >Analytical Results Graph</NavLink>
+                                    <NavLink
+                                        to={`/app/sites/${site.get('id')}/reports/groundwater-map`}
+                                        className="nav-link"
+                                        activeClassName="active"
+                                    >Groundwater Elevation Map</NavLink>
+                                    <NavLink
+                                        to={`/app/sites/${site.get('id')}/reports/thickness`}
+                                        className="nav-link"
+                                        activeClassName="active"
+                                    >Free Product Thickness</NavLink>
+                                    <NavLink
+                                        to={`/app/sites/${site.get('id')}/reports/groundwater`}
+                                        className="nav-link"
+                                        activeClassName="active"
+                                    >Free Product Groundwater</NavLink>
+                                    <NavLink
+                                        to={`/app/sites/${site.get('id')}/reports/wells`}
+                                        className="nav-link"
+                                        activeClassName="active"
+                                    >Well Construction/Location</NavLink>
+                                </nav>
+                            )}
+                        />
                         <NavLink
                             exact
                             to={`/app/sites/${site.get('id')}`}
