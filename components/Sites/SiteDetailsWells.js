90261837297f5c6dd28f5b01df4f26b8445cc090
Nick Woodhams <me@nickwoodhams.com>
2017-04-05 22:34:40 -0400

    site bulk uploads working

--- components/Sites/SiteDetailsWells.js
+++ components/Sites/SiteDetailsWells.js
@@ -1,6 +1,7 @@
 
 import React from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 import { Button, Table } from 'reactstrap'
 import filestack from 'filestack-js'
 import timeago from 'timeago.js'
@@ -11,7 +12,8 @@ import {
     patchUpload,
     deleteUpload,
 } from '../../actions/uploads'
-import { currentLab } from '../../normalizers'
+import { fetchSite } from '../../actions/sites'
+import { currentLab, currentCompany } from '../../normalizers'
 
 const FILESTACK_API_KEY = 'ATg3pguKNRI2jg6wRHiydz'
 const FILESTACK_OPTIONS = {
@@ -24,9 +26,6 @@ class SiteDetailsWells extends React.Component {
         super(props)
 
         this.client = filestack.init(FILESTACK_API_KEY)
-        this.state = {
-            sent: false,
-        }
     }
 
     componentDidMount () {
@@ -40,63 +39,73 @@ class SiteDetailsWells extends React.Component {
     }
 
     onUpload (res) {
-        res.filesUploaded.map(file =>
-            this.props.createUpload({
-                filename: file.filename,
-                url: file.url,
-                lab_id: this.props.lab.get('id'),
-                company_id: this.props.site.get('company_id'),
-                project_id: this.props.site.get('project_id'),
-                site_id: this.props.site.get('id'),
-                upload_type: 'well_data',
-            }))
-    }
-
-    onSend (upload) {
-        this.props.patchUpload(upload.get('id'), { sent: true })
-    }
-
-    removeItem (upload) {
-        this.props.deleteUpload(upload.get('id'))
+        const file = res.filesUploaded[0]
+        this.props.createUpload({
+            filename: file.filename,
+            url: file.url,
+            lab_id: this.props.lab.get('id'),
+            company_id: this.props.company.get('id'),
+            project_id: this.props.site.get('project_id'),
+            site_id: this.props.site.get('id'),
+            upload_type: 'well_data',
+        }).then(() => {
+            this.props.fetchSite(this.props.site.get('id'))
+        })
     }
 
     render () {
         const uploads = this.props.uploads
-            .filter(upload => upload.get('company_id') === this.props.site.get('company_id'))
-            .sort((a, b) => a.get('id') - b.get('id'))
-            .entrySeq()
+        .entrySeq()
 
         return (
             <div className="site-details-wells">
                 <div className="d-flex flex-row">
-                    <h4>Wells</h4>
-                    <Button
-                        color="secondary"
-                        role="button"
-                        className="ml-auto"
-                        onClick={() => this.onNewUpload()}
-                    >Bulk Upload</Button>
+                    <h2>Wells</h2>
                 </div>
-                <Table size="sm" style={{ marginTop: 30, marginBottom: 60 }}>
-                    <thead>
-                        <tr>
-                            <th>Filename</th>
-                            <th>Uploaded</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {uploads.map(([id, upload]) => (
-                            <tr key={id}>
-                                <td>
-                                    <a href={upload.get('url')} target="_blank" rel="noopener noreferrer">
-                                        {upload.get('filename')}
-                                    </a>
-                                </td>
-                                <td>{timeago().format(new Date(upload.get('created_at')))}</td>
+                <div className="well-list">
+                    {this.props.site.get('wells').map(well => (
+                        <div key={well.id}>
+                            <Link to={`/app/sites/${this.props.site.get('id')}/details/wells/${well.id}`}>{well.title}</Link>
+                        </div>
+                    ))}
+                </div>
+
+                <div className="bulk-upload">
+
+                    <div className="row justify-content-between">
+                        <div className="col-4">
+                            <h4>Bulk Upload</h4>
+                        </div>
+                        <div className="col-4 right">
+                            <Button
+                                color="secondary"
+                                role="button"
+                                className="ml-auto btn-bulk-upload"
+                                onClick={() => this.onNewUpload()}
+                            >Choose File</Button>
+                        </div>
+                    </div>
+                    <Table size="sm" style={{ marginTop: 30, marginBottom: 60 }}>
+                        <thead>
+                            <tr>
+                                <th>Filename</th>
+                                <th>Uploaded</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </Table>
+                        </thead>
+                        <tbody>
+                            {uploads.map(([id, upload]) => (
+                                <tr key={id}>
+                                    <td>
+                                        <a href={upload.get('url')} target="_blank" rel="noopener noreferrer">
+                                            {upload.get('filename')}
+                                        </a>
+                                    </td>
+                                    <td>{timeago().format(new Date(upload.get('created_at')))}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </Table>
+                </div>
             </div>
         )
     }
@@ -105,6 +114,7 @@ class SiteDetailsWells extends React.Component {
 const mapStateToProps = store => ({
     uploads: store.get('uploads'),
     lab: currentLab(store),
+    company: currentCompany(store),
 })
 
 const mapDispatchToProps = dispatch => ({
@@ -112,6 +122,7 @@ const mapDispatchToProps = dispatch => ({
     createUpload: upload => dispatch(createUpload(upload)),
     patchUpload: (id, upload) => dispatch(patchUpload(id, upload)),
     deleteUpload: id => dispatch(deleteUpload(id)),
+    fetchSite: id => dispatch(fetchSite(id)),
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(SiteDetailsWells)
