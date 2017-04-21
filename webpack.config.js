5790c90b083fedb805fbc7b1fbe1a4dbf6bf54bd
AJ <hi@ajain.me>
2017-04-21 15:41:48 -0700

    got basics of webpack working

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var path = require('path');
+var webpack = require('webpack');
+
+var config = {
+  entry: './index.js',
+  output: {
+    path: __dirname + '/dist/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          cacheDirectory: true,
+          presets: ['react', 'es2015']
+        }
+      },
+      {
+        test: /\.scss$/,
+        exclude: /\.js$/,
+        use: [{
+            loader: "style-loader" // creates style nodes from JS strings
+        }, {
+            loader: "css-loader" // translates CSS into CommonJS
+        }, {
+            loader: "sass-loader" // compiles Sass to CSS
+        }]
+    }]
+  },
+}
+
+
+module.exports = config
