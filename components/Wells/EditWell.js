b372130b20bfcf65896bdd06a9ec31e9a11ceb55
AJ <hi@ajain.me>
2017-04-27 01:43:08 -0700

    moved well form to using redux

--- components/Wells/EditWell.js
+++ components/Wells/EditWell.js
@@ -1,5 +1,4 @@
 
-
 import React from 'react'
 import { connect } from 'react-redux'
 import {
@@ -8,66 +7,75 @@ import {
     Button,
 } from 'reactstrap'
 
-import { fetchWell, deleteWell } from '../../actions/wells'
-import EditWellForm from './EditWellForm'
+import {
+  fetchWell,
+  deleteWell,
+  editWell,
+  clearEditingWellError,
+} from '../../actions/wells'
+import WellForm from './WellForm'
 
 
 class EditWell extends React.Component {
-    constructor (props) {
-        super(props)
+  constructor (props) {
+    super(props)
 
-        const wellId = parseInt(props.match.params.id, 10)
+    this.onSubmit = this.onSubmit.bind(this)
+  }
 
-        this.state = {
-            wellId,
-        }
-    }
+  componentDidMount () {
+    this.props.fetchWell(this.props.wellId)
+  }
 
-    componentDidMount () {
-        this.props.fetchWell(this.state.wellId)
-    }
+  onDelete () {
+    this.props.deleteWell(this.props.wellId)
+      .then(this.props.onSuccess)
+  }
 
-    onDelete () {
-        this.props.deleteWell(this.state.wellId)
-            .then(() => this.props.onSuccess())
-    }
+  onSubmit (wellParams) {
+    this.props.editWell(this.props.wellId, wellParams)
+  }
 
-    render () {
-        const well = this.props.wells.get(this.state.wellId)
+  render () {
+    const well = this.props.wells.get(this.props.wellId)
+    if (!well) { return null }
 
-        if (!well) {
-            return null
-        }
-
-        return (
-            <Row>
-                <Col sm={6}>
-                    <div className="d-flex">
-                        <h4>Edit Well</h4>
-                        <Button
-                            onClick={() => this.onDelete()}
-                            className="ml-auto"
-                            role="button"
-                            color="danger"
-                        >Delete Well</Button>
-                    </div>
-                    <EditWellForm
-                        well={well}
-                        onSuccess={this.props.onSuccess}
-                    />
-                </Col>
-            </Row>
-        )
-    }
+    return (
+      <Row>
+        <Col sm={6}>
+          <div className="d-flex">
+            <h4>Edit Well</h4>
+            <Button
+              onClick={() => this.onDelete()}
+              className="ml-auto"
+              role="button"
+              color="danger"
+            >Delete Well</Button>
+          </div>
+          <WellForm
+            initialValues={well}
+            submitForm={this.onSubmit}
+            wellError={this.props.editingWellError}
+            clearWellError={this.props.clearEditingWellError}
+          />
+        </Col>
+      </Row>
+    )
+  }
 }
 
-const mapStateToProps = store => ({
+const mapStateToProps = (store, props) => ({
     wells: store.get('wells'),
+    editingWellError: store.get('editingWellError'),
+    editingWell: store.get('editingWell'),
+    wellId: parseInt(props.match.params.id, 10),
 })
 
 const mapDispatchToProps = dispatch => ({
     fetchWell: id => dispatch(fetchWell(id)),
     deleteWell: id => dispatch(deleteWell(id)),
+    editWell: (id, well) => dispatch(editWell(id, well)),
+    clearEditingWellError: () => dispatch(clearEditingWellError()),
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(EditWell)
