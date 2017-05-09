e67a5d588bd41b96417f08abbc435282c12c0a2c
Nick Woodhams <me@nickwoodhams.com>
2017-05-08 20:02:11 -0400

    completed some sidebar and header styles

--- components/SidebarHeader.js
+++ components/SidebarHeader.js
@@ -128,6 +128,15 @@ class Header extends React.Component {
       )
     }
 
+    if (this.props.sites) {
+      console.log(this.props.sites)
+      if (this.props.sites.size) {
+        siteTitle = this.props.sites.get(this.state.siteId).get('title')
+      }
+    } else {
+      siteTitle = ''
+    }
+
     return (
       <div className="navbar-container">
         {flashAlert}
@@ -135,7 +144,8 @@ class Header extends React.Component {
           className="flex-row justify-content-end"
           style={{ marginBottom: 20 }}
         >
-          <Link to="/app" className="mr-auto navbar-brand">{this.getAppTitle()}</Link>
+          <Link to="/app" className="mr-auto navbar-brand navbar-sidebar">{this.getAppTitle()}</Link>
+          <div className="site-name">{siteTitle}</div>
           {userDropdown}
         </Navbar>
       </div>
