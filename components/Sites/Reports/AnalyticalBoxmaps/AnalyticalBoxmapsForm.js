6fa234c1e2098807f2a3aa2554aa635251661af5
Anshul Jain <hi@ajain.me>
2017-05-11 23:01:32 -0700

    analytical boxmaps form complete

--- components/Sites/Reports/AnalyticalBoxmaps/AnalyticalBoxmapsForm.js
+++ components/Sites/Reports/AnalyticalBoxmaps/AnalyticalBoxmapsForm.js
@@ -5,6 +5,7 @@ import moment from 'moment'
 import {
   Form,
   Input,
+  Button,
 } from 'reactstrap'
 
 import SelectFormGroup from 'SharedComponents/ReduxFormHelpers/SelectFormGroup'
@@ -16,6 +17,7 @@ import {
 import { fetchSiteMaps } from 'actions/siteMaps'
 import { fetchSamples } from 'actions/samples'
 import { fetchCriterias } from 'actions/criterias'
+import { editSite } from 'actions/sites'
 
 class AnalyticalBoxmapsForm extends React.Component {
   componentDidMount () {
@@ -25,6 +27,19 @@ class AnalyticalBoxmapsForm extends React.Component {
     this.props.fetchSubstances()
     this.props.fetchSubstanceGroups()
   }
+
+  addSubstance(evt) {
+    this.props.editSite(this.props.site.get('id'), {
+      'substances': { 'add': [evt.target.value], 'remove': []}
+    })
+  }
+
+  removeSubstance(substanceId) {
+    this.props.editSite(this.props.site.get('id'), {
+      'substances': { 'add': [], 'remove': [substanceId] }
+    })
+  }
+
   render () {
     const {
       site,
@@ -38,97 +53,78 @@ class AnalyticalBoxmapsForm extends React.Component {
     const siteMapOptions = siteMaps.valueSeq().map((siteMap) =>
       <option key={siteMap.get('id')} value={siteMap.get('id')}>{siteMap.get('title')}</option>)
 
-    const dateOptions = dates.map((date, i) =>
+    const dateOptions = dates.valueSeq().map((date, i) =>
       <option key={i}>{date.format('YYYY-MM-DD')}</option>)
 
-    const criteriaOptions = criterias.map((criteria) =>
+    const criteriaOptions = criterias.valueSeq().map((criteria) =>
       <option key={criteria.get('id')} value={criteria.get('id')}>{criteria.get('title')}</option>)
 
     const groupedSubstances = substanceGroups.map((substanceGroup) =>
       substances.filter((substance) => (substance.get('substance_group_id') === substanceGroup.get('id')))
     )
 
-    // debugger
-
-    // if (substances.size) { debugger }
-
     const groupedSubstanceOptions = groupedSubstances.map((substances, substanceGroupId) =>
       <optgroup key={substanceGroupId} label={substanceGroups.get(substanceGroupId).get('title')}>
         {substances.valueSeq().map(substance =>
-          (<option value={substance.get('id')}>{substance.get('title')}</option>))}
+          (<option key={substance.get('id')} value={substance.get('id')}>{substance.get('title')}</option>))}
       </optgroup>
     ).valueSeq()
 
-    const siteSubstances = site.get('substance_ids').map((id) =>
-      <li key={id}>
-        {substance.get(id).get('title')}
-        <a href='#'> X </a>
-      </li>
-    )
+    const siteSubstances = site.get('substance_ids').map((id) => {
+      if (!substances.size) { return null }
+      return (<li key={id}>
+        {substances.get(id).get('title')}
+        <a href='#' onClick={(e) => this.removeSubstance(id)}> X </a>
+      </li>)
+    })
     return (
       <div>
-      <Form>
-        <Field
-          props={{label: 'Site Map'}}
-          name='sitemap'
-          id='sitemap'
-          options={siteMapOptions}
-          component={SelectFormGroup}
-        />
+        <Form>
+          <Field
+            props={{label: 'Site Map'}}
+            name='sitemap'
+            id='sitemap'
+            options={siteMapOptions}
+            component={SelectFormGroup}
+          />
 
-        <Field
-          props={{label: 'Start Date'}}
-          name='start_date'
-          id='start_date'
-          options={dateOptions}
-          component={SelectFormGroup}
-         />
+          <Field
+            props={{label: 'Start Date'}}
+            name='start_date'
+            id='start_date'
+            options={dateOptions}
+            component={SelectFormGroup}
+           />
 
-        <Field
-          props={{label: 'End Date'}}
-          name='end_date'
-          id='end_date'
-          options={dateOptions}
-          component={SelectFormGroup}
-        />
+          <Field
+            props={{label: 'End Date'}}
+            name='end_date'
+            id='end_date'
+            options={dateOptions}
+            component={SelectFormGroup}
+          />
 
-        <Field
-          props={{label: 'Comparison Criteria'}}
-          name='criteria_id'
-          id='criteria_id'
-          options={criteriaOptions}
-          component={SelectFormGroup}
-        />
+          <Field
+            props={{label: 'Comparison Criteria'}}
+            name='criteria_id'
+            id='criteria_id'
+            options={criteriaOptions}
+            component={SelectFormGroup}
+          />
 
-        Substances
-        <ul>
-          {siteSubstances}
-        </ul>
+          Substances
+          <ul>
+            {siteSubstances}
+          </ul>
 
-        <Input type='select'>
-          {groupedSubstanceOptions}
-        </Input>
-      </Form>
-        Criterias
-        <ul>
-          {this.props.criterias.valueSeq().map((sub) => <li key={sub.get('id')}>{sub.get('title')}</li>)}
-        </ul>
-        Samples
-        <ul>
-          {this.props.dates.valueSeq().map((sub, i) => <li key={i}>{sub.format('YYYY-MM-DD')}</li>)}
-        </ul>
-        Substances
-        <ul>
-          {this.props.substances.valueSeq().map((sub) => <li key={sub.get('id')}>{sub.get('title')}</li>)}
-        </ul>
-        Substance Groups
-        <ul>
-          {this.props.substanceGroups.valueSeq().map((sub) => <li key={sub.get('id')}>{sub.get('title')}</li>)}
-        </ul>
-        SiteMaps
-        <ul>
-          {this.props.siteMaps.valueSeq().map((sitemap) => <li key={sitemap.get('id')}>{sitemap.get('title')}</li>)}
-        </ul>
+          <Input type='select' onChange={this.addSubstance.bind(this)}>
+            {groupedSubstanceOptions}
+          </Input>
+
+          <Button
+            color="primary"
+          >Save</Button>
+        </Form>
       </div>
     )
   }
@@ -151,7 +147,9 @@ const mapDispatchToProps = (dispatch) => ({
   fetchSubstanceGroups: () => dispatch(fetchSubstanceGroups()),
   fetchSiteMaps: filters => dispatch(fetchSiteMaps(filters)),
   fetchSamples: filters => dispatch(fetchSamples(filters)),
-  fetchCriterias: filters => dispatch(fetchCriterias(filters))
+  fetchCriterias: filters => dispatch(fetchCriterias(filters)),
+
+  editSite: (id, siteParams) => dispatch(editSite(id, siteParams))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(AnalyticalBoxmapsForm)
