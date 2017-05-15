b0900aec828662fd915287f10d56afad9b625a45
Nick Woodhams <me@nickwoodhams.com>
2017-05-15 01:57:43 -0400

    made sure each view has a consistent title style, switched tabspacing for many files

--- components/Sites/SiteDetails/Schedule/SchedulesList.js
+++ components/Sites/SiteDetails/Schedule/SchedulesList.js
@@ -5,71 +5,76 @@ import { Link } from 'react-router-dom'
 import moment from 'moment'
 
 import {
-    Button,
+  Button,
 } from 'reactstrap'
 
 import {
-    createSchedule,
-    clearCreatingScheduleError,
-    fetchSchedules,
+  createSchedule,
+  clearCreatingScheduleError,
+  fetchSchedules,
 } from 'actions/schedule'
 import { msgFromError } from 'util'
 
 class SchedulesList extends React.Component {
-    constructor (props) {
-        super(props)
+  constructor (props) {
+    super(props)
+  }
+
+  componentWillMount () {
+    this.props.fetchSchedules({site_id: this.props.site.get('id')})
+  }
+
+  componentDidMount () {
+    this.props.fetchSchedules({site_id: this.props.site.get('id')})
+    .then(() => {
+      this.props.schedules.map(schedule => {
+        console.log(schedule.get('date'))
+      })
+    })
+
+  }
+
+  onChange (e) {
+  }
+
+
+  onSubmit (e) {
+  }
+
+  render () {
+    let schedules = undefined
+
+    if (this.props.schedules.size > 0 && this.props.site) {
+      schedules = this.props.schedules.map((schedule) => {
+        return <li>
+          <Link to={`/app/sites/${this.props.site.get('id')}/details/sample-schedule/${schedule.get('id')}`}> {moment(schedule.get('date')).utc().format('YYYY-MM-DD')} </Link>
+        </li>
+      })
+    } else {
+      schedules = <span> No schedules yet. <Link to={`/app/sites/${this.props.site.get('id')}/details/sample-schedule/new`}>Create one</Link> </span>
     }
 
-    componentWillMount () {
-        this.props.fetchSchedules({site_id: this.props.site.get('id')})
-    }
-
-    componentDidMount () {
-        this.props.fetchSchedules({site_id: this.props.site.get('id')})
-        .then(() => {
-            this.props.schedules.map(schedule => {
-                console.log(schedule.get('date'))
-            })
-        })
-
-    }
-
-    onChange (e) {
-    }
-
-
-    onSubmit (e) {
-    }
-
-    render () {
-        let schedules = undefined
-
-        if (this.props.schedules.size > 0 && this.props.site) {
-            schedules = this.props.schedules.map((schedule) => {
-              return <li>
-                  <Link to={`/app/sites/${this.props.site.get('id')}/details/sample-schedule/${schedule.get('id')}`}> {moment(schedule.get('date')).utc().format('YYYY-MM-DD')} </Link>
-              </li>
-            })
-        } else {
-            schedules = <span> No schedules yet. <Link to={`/app/sites/${this.props.site.get('id')}/details/sample-schedule/new`}>Create one</Link> </span>
-        }
-
-        return (
-            <div className="sample-schedule">
-                <Button onClick={() => this.props.push(`/app/sites/${this.props.site.get('id')}/details/sample-schedule/new`)} className='btn btn-default float-right'>New Schedule</Button>
-                <h2>Site Details Schedule List</h2>
-                {schedules}
-            </div>
-        )
-    }
+    return (
+      <div className="sample-schedule">
+        <div className="d-flex flex-row">
+          <h2>Site Details Schedule List</h2>
+          <Button
+            onClick={() => this.props.push(`/app/sites/${this.props.site.get('id')}/details/sample-schedule/new`)}
+            className="ml-auto btn btn-default"
+          >New Schedule</Button>
+        </div>
+        {schedules}
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = store => ({
-    schedules: store.get('schedules'),
+  schedules: store.get('schedules'),
 })
 
 const mapDispatchToProps = dispatch => ({
-    fetchSchedules: filters => dispatch(fetchSchedules(filters)),
+  fetchSchedules: filters => dispatch(fetchSchedules(filters)),
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(SchedulesList)
