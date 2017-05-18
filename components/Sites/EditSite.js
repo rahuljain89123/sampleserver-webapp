315bd2b88079dc55ec5b9e0c4241445ac491cf0c
Anshul Jain <hi@ajain.me>
2017-05-18 16:37:49 -0700

    used redux form for editing/creating a site, added flash messages

--- components/Sites/EditSite.js
+++ components/Sites/EditSite.js
@@ -1,20 +1,73 @@
 
 import React from 'react'
+import { connect } from 'react-redux'
 import {
   Row,
   Col,
 } from 'reactstrap'
 
-import EditSiteForm from './EditSiteForm'
+import {
+  editSite,
+  clearEditingSiteError,
+  setEditingSite,
+} from 'actions/sites'
+import { flashMessage } from 'actions/global'
 
-const EditSite = props => (
-  <Row>
-    <Col sm={6}>
-      <EditSiteForm
-        site={props.site}
-      />
-    </Col>
-  </Row>
-)
+import SiteForm from './SiteForm'
 
-export default EditSite
+class EditSite extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.onSubmitSiteForm = this.onSubmitSiteForm.bind(this)
+  }
+
+  onSubmitSiteForm (siteParams) {
+    siteParams = siteParams.update('state_id', (v) => parseInt(v))
+
+    this.props.editSite(this.props.site.get('id'), siteParams)
+      .then(() => this.props.flashMessage('success', 'Site updated successfully'))
+      .catch(() => this.props.flashMessage('danger', 'Sorry, there was an error.'))
+  }
+
+  render () {
+    const {
+      site,
+      editingSite,
+      editingSiteError,
+      clearEditingSiteError,
+    } = this.props
+
+    if (!site) { return null }
+
+    return (
+      <Row>
+        <Col sm={6}>
+          <SiteForm
+            initialValues={site}
+            siteError={editingSiteError}
+            clearSiteError={clearEditingSiteError}
+            submittingForm={editingSite}
+            submitForm={this.onSubmitSiteForm}
+          />
+        </Col>
+      </Row>
+    )
+  }
+
+}
+
+const mapStateToProps = (state, props) => ({
+  editingSite: state.get('editingSite'),
+  editingSiteError: state.get('editingSiteError'),
+})
+
+const mapDispatchToProps = dispatch => ({
+  flashMessage: (type, message) => dispatch(flashMessage(type, message)),
+
+  editSite: (siteId, siteParams) => dispatch(editSite(siteId, siteParams)),
+  setEditingSite: (editing) => dispatch(setEditingSite(editing)),
+  clearEditingSiteError: () => dispatch(clearEditingSiteError()),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(EditSite)
