aafac54825dd05256bc6d4189fbb1a097aaf392b
Anshul Jain <hi@ajain.me>
2017-06-20 15:44:28 -0700

    foreach instaed of map

--- components/Clients/ClientSites.js
+++ components/Clients/ClientSites.js
@@ -33,7 +33,7 @@ class ClientSites extends React.Component {
 
   componentDidMount () {
     this.props.fetchClients().then(() =>
-      this.props.clients.map(client => this.props.fetchSites({ client_id: client.get('id'), per_page: 150 }))
+      this.props.clients.forEach(client => this.props.fetchSites({ client_id: client.get('id'), per_page: 150 }))
     )
     this.props.setHeaderInfo(
       'Dashboard',
